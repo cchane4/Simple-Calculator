6dd93727dddb409a264f32c33fd14257991c974e
Chad Chaney <cchane4@gmail.com>
2018-02-17 17:51:11 -0600

    first commit node operation built

---
+++ calculator.js
@@ -0,0 +1,30 @@
+const input = process.argv;
+console.log(input);
+
+/*
+operator = input [2];
+let num1 = input [3];
+let  num2 = input [4];
+
+if (operator == 'add'){
+    console.log(parseFloat(num1) + parseFloat(num2));
+}
+else if (operator == 'subtract'){
+    console.log(parseFloat(num1) - parseFloat(num2));
+}
+else if (operator == 'multiply'){
+    console.log(parseFloat(num1) * parseFloat(num2));
+}
+else if (operator == 'divide'){
+    console.log(parseFloat(num1) / parseFloat(num2));
+}
+else if (operator == 'exponent'){
+    console.log(parseFloat(num1) ^ parseFloat(num2));
+}
+else if  (operator == 'remainder'){
+    console.log(parseFloat(num1) % parseFloat(num2));
+}
+
+else { console.log('Operator Not Found!!')
+}
+*/

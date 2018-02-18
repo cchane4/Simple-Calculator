33ee0c58d7620212c03e2fd9c5f41f989cdd5053
Chad Chaney <cchane4@gmail.com>
2018-02-17 20:51:50 -0600

    making code more modular

--- ifelse.js
+++ ifelse.js
@@ -1,30 +1,30 @@
 const input = process.argv;
-console.log(input);
 
 
 operator = input [2];
-let num1 = input [3];
-let  num2 = input [4];
+let num1 = parseFloat(input [3]);
+let  num2 = parseFloat(input [4]);
+
 
 if (operator == 'add'){
-    console.log(parseFloat(num1) + parseFloat(num2));
+    console.log((num1) + (num2));
 }
 else if (operator == 'subtract'){
-    console.log(parseFloat(num1) - parseFloat(num2));
+    console.log((num1) - (num2));
 }
 else if (operator == 'multiply'){
-    console.log(parseFloat(num1) * parseFloat(num2));
+    console.log((num1) * (num2));
 }
 else if (operator == 'divide'){
-    console.log(parseFloat(num1) / parseFloat(num2));
+    console.log((num1) / (num2));
 }
 else if (operator == 'exponent'){
-    console.log(parseFloat(num1) ^ parseFloat(num2));
+    console.log((num1) ^ (num2));
 }
 else if  (operator == 'remainder'){
-    console.log(parseFloat(num1) % parseFloat(num2));
+    console.log((num1) % (num2));
 }
 
-else { console.log('Operator Not Found!!')
+else { console.log('Enter a valid operator and two numbers!!')
 }
 
--- switchcase.js
+++ switchcase.js
@@ -1,38 +1,37 @@
 const input = process.argv;
-console.log(input);
 
 
 operator = input [2];
-let num1 = input [3];
-let  num2 = input [4
+let num1 = parseFloat(input [3]);
+let  num2 = parseFloat (input [4]);
 
 switch (operator){
 case "add":
-    console.log(parseFloat(num1) + parseFloat(num2));
+    console.log((num1) + (num2));
 break;
 
 case "subtract":
-    console.log(parseFloat(num1) - parseFloat(num2));
+    console.log((num1) - (num2));
 break;
 
 case "multiply":
-    console.log(parseFloat(num1) * parseFloat(num2));
+    console.log((num1) * (num2));
 break;
 
 case "divide":
-    console.log(parseFloat(num1) / parseFloat(num2));
+    console.log((num1) / (num2));
 break;
 
 case "exponent":
-    console.log(parseFloat(num1) ^ parseFloat(num2));
+    console.log((num1) ^ (num2));
 break;
 
 case "remainder":
-    console.log(parseFloat(num1) % parseFloat(num2));
+    console.log((num1) % (num2));
 break;
 
 default:
-console.log('Enter an operator and two numbers!!');
+console.log('Enter a valid operator and two numbers!!');
 break;
 }
 

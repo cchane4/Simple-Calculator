963a8ee6fbe664cae37ac80732d9e00c61d265e1
Chad Chaney <cchane4@gmail.com>
2018-02-17 20:32:23 -0600

    adding files and organizing

--- switchcase.js
+++ switchcase.js
@@ -1,30 +1,38 @@
 const input = process.argv;
 console.log(input);
 
-/*
+
 operator = input [2];
 let num1 = input [3];
-let  num2 = input [4];
+let  num2 = input [4
 
-if (operator == 'add'){
+switch (operator){
+case "add":
     console.log(parseFloat(num1) + parseFloat(num2));
-}
-else if (operator == 'subtract'){
+break;
+
+case "subtract":
     console.log(parseFloat(num1) - parseFloat(num2));
-}
-else if (operator == 'multiply'){
+break;
+
+case "multiply":
     console.log(parseFloat(num1) * parseFloat(num2));
-}
-else if (operator == 'divide'){
+break;
+
+case "divide":
     console.log(parseFloat(num1) / parseFloat(num2));
-}
-else if (operator == 'exponent'){
+break;
+
+case "exponent":
     console.log(parseFloat(num1) ^ parseFloat(num2));
-}
-else if  (operator == 'remainder'){
+break;
+
+case "remainder":
     console.log(parseFloat(num1) % parseFloat(num2));
+break;
+
+default:
+console.log('Enter an operator and two numbers!!');
+break;
 }
 
-else { console.log('Operator Not Found!!')
-}
-*/
